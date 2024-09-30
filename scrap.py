3b413200667cbe6b92ef508728d38b187eb3f28e
sofianeharsal <haarsal@gmail.com>
2024-09-30 13:58:35 +0200

    delete env/

---
+++ scrap.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+
+    links = job_element.find_all("a")
+    for link in links:
+     if "Apply" in link.text :
+        link_url = link["href"]
+        print(f"Apply here: {link_url}\n")
